e62ff0ec596616fd3e980bdb2f72523c7fe9b0d7
YvainP <i20-10@hotmail.fr>
2020-03-15 20:33:07 +0100

    debut

---
+++ main.py
@@ -0,0 +1,13 @@
+from datetime import datetime 
+import time
+# Alarm = input("What time do you want to wake up?\n")
+#now = datetime.now()
+#currentTime = now.strftime("%H:%M")
+
+#while currentTime != Alarm:
+ #   now = datetime.now()
+#    currentTime = now.strftime("%H:%M")
+#    print("The time is " + currentTime)
+#    if currentTime == Alarm:
+#        print("Time to wake up!")
+

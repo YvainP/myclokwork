6d6dab36938c317688bf111be7035d03eada1101
YvainP <i20-10@hotmail.fr>
2020-05-05 17:30:58 +0200

    m

--- alarm.py
+++ alarm.py
@@ -1,11 +1,10 @@
 import datetime
-import os
 import time
-
+import playsound
 now = datetime.datetime.now()
 
-alarm_time = datetime.datetime.combine(now.date(), datetime.time(15, 25, 0))
+alarm_time = datetime.datetime.combine(now.date(), datetime.time(17, 8, 0))
 
 time.sleep((alarm_time - now).total_seconds())
 
-os.system("start test.mp3")
+playsound('test.mp3')
